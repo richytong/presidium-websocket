1a664b460313c46f0b12bf3ec73ad5f1e34b29f7
Richard <richytong@gmail.com>
2025-07-01 12:05:12 -0700

    dist

--- WebSocketServer.js
+++ WebSocketServer.js
@@ -1,5 +1,5 @@
 /**
- * presidium-websocket v0.0.5
+ * presidium-websocket v0.0.6
  * https://github.com/richytong/presidium-websocket
  * (c) 2025 Richard Tong
  * presidium-websocket may be freely distributed under the MIT license.
