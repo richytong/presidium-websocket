b19a3ea313de84825eb7a752f43e78f46a2bface
Richard <richytong@gmail.com>
2026-02-04 16:57:35 -0800

    docs

--- WebSocketSecureServer.js
+++ WebSocketSecureServer.js
@@ -15,44 +15,61 @@ const WebSocketServer = require('./WebSocketServer')
  * module http 'https://nodejs.org/api/http.html'
  * module net 'https://nodejs.org/api/net.html'
  *
- * websocketHandler (websocket WebSocket)=>()
- * httpHandler (request http.ClientRequest, response http.ServerResponse)=>()
- * upgradeHandler (request http.ClientRequest, socket net.Socket, head Buffer)=>()
+ * type WebSocketHandler = (websocket WebSocket)=>undefined
+ * type HTTPHandler = (request http.ClientRequest, response http.ServerResponse)=>undefined
+ *
+ * new WebSocketSecureServer(websocketHandler WebSocketHandler, options 
+ *   httpHandler: HTTPHandler,
+ *   key: string|Array<string>|Buffer|Array<Buffer>|Array<{
+ *     pem: string|Buffer,
+ *     passphrase: string
+ *   }>,
+ *   cert: string|Array<string>|Buffer|Array<Buffer>,
+ *   passphrase: string,
+ *   supportPerMessageDeflate: boolean,
+ *   maxMessageLength: number
+ *   socketBufferLength: number
+ * }) -> server WebSocketSecureServer
  *
  * new WebSocketSecureServer(options {
- *   key: string,
- *   cert: string,
- *   passphrase: string
+ *   websocketHandler: WebSocketHandler,
+ *   httpHandler: HTTPHandler,
+ *   key: string|Array<string>|Buffer|Array<Buffer>|Array<{
+ *     pem: string|Buffer,
+ *     passphrase: string
+ *   }>,
+ *   cert: string|Array<string>|Buffer|Array<Buffer>,
+ *   passphrase: string,
+ *   supportPerMessageDeflate: boolean,
+ *   maxMessageLength: number
+ *   socketBufferLength: number
  * }) -> server WebSocketSecureServer
+ * ```
  *
- * new WebSocketSecureServer(websocketHandler, options {
- *   httpHandler: httpHandler,
- *   key: string,
- *   cert: string,
- *   passphrase: string
- * }) -> server WebSocketSecureServer
+ * Presidium WebSocketSecureServer class.
  *
- * new WebSocketSecureServer(options {
- *   websocketHandler: websocketHandler,
- *   httpHandler: httpHandler,
- *   key: string,
- *   cert: string,
- *   passphrase: string
- * }) -> server WebSocketSecureServer
+ * Arguments:
+ *   * `websocketHandler` - a handler function that expects an instance of a [`ServerWebSocket`](/docs/ServerWebSocket). Represents the server's WebSocket connection to the client.
+ *   * `options`
+ *     * `httpHandler` - function that processes incoming HTTP requests from clients. Defaults to an HTTP handler that responds with `200 OK`.
+ *     * `key` - private key(s) in PEM format. Encrypted keys will be decrypted using the `passphrase` option. Multiple keys using different algorithms can be provided as an array of unencrypted key strings or buffers, or an array of objects in the form `{ pem: string|Buffer, passphrase: string }`.
+ *     * `cert` - cert chain(s) in PEM format. One cert chain should be provided per private key.
+ *     * `passphrase` - used to decrypt the private key(s).
+ *     * `supportPerMessageDeflate` - if `true`, indicates to WebSocket clients that the server supports [Compression Extensions for WebSocket](https://datatracker.ietf.org/doc/html/rfc7692). If an incoming WebSocket connection has requested compression extensions via the `Sec-WebSocket-Extensions: permessage-deflate` header, all messages exchanged in the WebSocket connection will be compressed using [zlib](https://nodejs.org/api/zlib.html) default options. Defaults to `false`.
+ *     * `maxMessageLength` - the maximum length in bytes of sent messages. If a message is longer than `maxMessageLength`, it is split into fragmented messages that are reassembled by the receiver.
+ *     * `socketBufferLength` - length in bytes of the internal buffer of the underlying [socket](https://nodejs.org/api/net.html#class-netsocket) for all connections to the server.
  *
- * server.on('connection', websocketHandler) -> ()
- * server.on('request', httpHandler) -> ()
- * server.on('upgrade', upgradeHandler) -> ()
- * server.on('error', (error Error)=>()) -> ()
- * server.on('close', ()=>()) -> ()
+ * Return:
+ *   * `server` - an instance of the WebSocketSecureServer.
  *
- * server.on('connection', (websocket WebSocket) => {
- *   websocket.on('open', ()=>()) -> ()
- *   websocket.on('message', (message Buffer)=>()) -> ()
- *   websocket.on('ping', ()=>()) -> ()
- *   websocket.on('pong', ()=>()) -> ()
- *   websocket.on('error', (error Error)=>()) -> ()
- *   websocket.on('close', ()=>()) -> ()
+ * ```javascript
+ * const server = new WebSocketSecureServer({
+ *   key: fs.readFileSync('/path/to/my-key'),
+ *   cert: fs.readFileSync('/path/to/my-cert'),
+ * })
+ *
+ * server.listen(4443, () => {
+ *   console.log('WebSocket secure server listening on port 4443')
  * })
  * ```
  */
